cd7e0dea65baebeb77390148208a96cf10023774
Ying Liu <yingl1984@gmail.com>
2020-12-29 13:19:54 -0800

    Send a Get request to Place Detail API

--- README.md
+++ README.md
@@ -1,16 +1,24 @@
 # Ask For Fun
-Users input an address, click the "Go" button, then the place will be marked in the Google map. Click the marker, users can see the detailed place information in the information window.
+2 unique features are provided within this project.
+Feature 1: Users input an address, say "Liberty Island New York, NY 10004", click the "Pinpoint" button, the place will be marked in the Google map. Click the marker, users can see the detailed place information in the information window.
+
+Feature 2: Users input something interested in, say "Museum in New York", click "Search" button, the result will be pinpointed on the map. Click the marker, users can see the detailed place information in the information window.
 
 ## Checkout the live website below
 [Ask For Fun](https://yingl1984.github.io/search_place/)
 
 ## Screenshots
-![screenshot](screenshot.png)
+### Pinpoint the place on map
+![screenshot](screenshot1.png)
+### Search place
+![screenshot](screenshot2.png)
 
-## 2 APIs used in this app
+## 3 APIs used in this app
 1. Google Map API endpoint: 
 https://maps.googleapis.com/maps/api
-2. Google Geocode API endpoint: 
+2. Google Place Details API endpoint:
+https://maps.googleapis.com/maps/api/place/findplacefromtext
+3. Google Geocode API endpoint: 
 https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/geocode
 
 ## Built With
--- index.js
+++ index.js
@@ -6,15 +6,24 @@ let map;
 let service;
 let infowindow;
 
-//Get the geolocation information by sending HTTP GET request to the Google API endpoint: https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/geocode
-function generateGeolocation(query){
-  fetch(`https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/geocode/json?address=${query}&key=${apiKey}`)
-  .then(response => response.json())
+function placeDetails(query)
+{
+  fetch(`https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=${query}&inputtype=textquery&fields=place_id,formatted_address,name,rating,opening_hours,geometry&key=${apiKey}`)
+  .then(response=>response.json())
   .then(response => {
-    setUpMap(response.results[0].geometry.location.lat, 
-      response.results[0].geometry.location.lng,
-      response.results[0].place_id,
-      response.results[0].formatted_address)
+    const isOpened = response.candidates[0].opening_hours.open_now == false ? "No" : "Yes";
+    const contentString =
+  "<div>" +
+  "<p><strong>Place name</strong>: "+response.candidates[0].name+
+  "<br><strong>Address</strong>: "+response.candidates[0].formatted_address+
+  "<br><strong>Rating</strong>: "+response.candidates[0].rating+
+  "<br><strong>Opened</strong>: "+isOpened+
+  "</p></div>";
+
+    setUpMap(response.candidates[0].geometry.location.lat, 
+      response.candidates[0].geometry.location.lng,
+      contentString
+      )
   })
   .catch(err => {
     $('#map').addClass('hidden');
@@ -23,8 +32,28 @@ function generateGeolocation(query){
   });
 }
 
-//Mark the place in the map and show the detail information in the infowindow after clicking the marker
-function setUpMap(lat, lng, placeId, formattedAdd)
+function generateGeolocation(query){
+  fetch(`https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/geocode/json?address=${query}&key=${apiKey}`)
+  .then(response => response.json())
+  .then(response => {
+    const contentString =
+    "<div>" +
+    "<p><strong>Address</strong>: "+response.results[0].formatted_address+
+    "</p></div>";
+    setUpMap(response.results[0].geometry.location.lat, 
+      response.results[0].geometry.location.lng,
+      contentString
+      )
+  })
+  .catch(err => {
+    $('#map').addClass('hidden');
+    $('#js-error-message').removeClass('hidden');
+    $('#js-error-message').text(`Something went wrong: ${err.message}`);
+  });
+}
+
+//Map
+function setUpMap(lat, lng, contentString)
 {
   const latLng = new google.maps.LatLng(lat, lng);
 
@@ -36,17 +65,11 @@ function setUpMap(lat, lng, placeId, formattedAdd)
 
   let marker = new google.maps.Marker({
     position: latLng,
-    title:"Search fun stuff!",
+    title:"Click to see the place detail information",
     visible: true
   });
 
   map = new google.maps.Map(document.getElementById("map"), mapOptions);
-  
-  const contentString =
-  "<div>" +
-  "<p><b>Place id</b>: "+placeId+
-  "<br><b>Address</b>: "+formattedAdd+
-  "</p></div>";
   
   const infowindow = new google.maps.InfoWindow({
     content: contentString
@@ -55,18 +78,25 @@ function setUpMap(lat, lng, placeId, formattedAdd)
     infowindow.open(map, marker);
   });
   marker.setMap(map);
-
 }
 
 function watchForm() {
-  $("form").submit((e)=>{
-    e.preventDefault();
+  //Pinpoint the place on map
+  $("#js-form").submit((e)=>{
     $('#map').removeClass('hidden');
     $('#js-error-message').addClass('hidden');
+    e.preventDefault();
     const searchItem = $('#js-search-item').val();
     generateGeolocation(searchItem);
-  })
-  
+  });
+  // Search place users interested in
+  $("#place-form").submit((e)=>{
+    $('#map').removeClass('hidden');
+    $('#js-error-message').addClass('hidden');
+    e.preventDefault();
+    const placeText = $("#place-item").val();
+    placeDetails(placeText);
+  }) 
 }
 
 $(watchForm);
